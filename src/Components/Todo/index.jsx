775b7becff8514a78815e2e8b975a691cd6a283c
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-05-15 23:23:19 -0700

    work in progress

--- src/Components/Todo/index.jsx
+++ src/Components/Todo/index.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import useForm from '../../hooks/form';
+import React, { useEffect, useState } from "react";
+import useForm from "../../hooks/form";
+import { Button, TextInput } from "@mantine/core";
 
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 const Todo = () => {
-
   const [defaultValues] = useState({
     difficulty: 4,
   });
@@ -20,31 +20,29 @@ const Todo = () => {
   }
 
   function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
+    const items = list.filter((item) => item.id !== id);
     setList(items);
   }
 
   function toggleComplete(id) {
-
-    const items = list.map( item => {
-      if ( item.id === id ) {
-        item.complete = ! item.complete;
+    const items = list.map((item) => {
+      if (item.id === id) {
+        item.complete = !item.complete;
       }
       return item;
     });
 
     setList(items);
-
   }
 
   useEffect(() => {
-    let incompleteCount = list.filter(item => !item.complete).length;
+    let incompleteCount = list.filter((item) => !item.complete).length;
     setIncomplete(incompleteCount);
     document.title = `To Do List: ${incomplete}`;
-    // linter will want 'incomplete' added to dependency array unnecessarily. 
-    // disable code used to avoid linter warning 
-    // eslint-disable-next-line react-hooks/exhaustive-deps 
-  }, [list]);  
+    // linter will want 'incomplete' added to dependency array unnecessarily.
+    // disable code used to avoid linter warning
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [list]);
 
   return (
     <>
@@ -53,39 +51,81 @@ const Todo = () => {
       </header>
 
       <form onSubmit={handleSubmit}>
-
         <h2>Add To Do Item</h2>
+        <TextInput
+          onChange={handleChange}
+          name="text"
+          type="text"
+          placeholder="Item Details"
+          label="To Do Item"
+          description="Enter in an item you want to add to your todo list"
+          radius="lg"
+          size="md"
+          withAsterisk
+        />
 
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
+        <TextInput
+          placeholder="Assignee Name"
+          label="Assigned To"
+          description="Who do you want to assign this task to?"
+          radius="lg"
+          size="md"
+          withAsterisk
+          name="assignee"
 
-        <label>
+        />
+        {/* <label>
           <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
+          <input
+            onChange={handleChange}
+            name="assignee"
+            placeholder="Assignee Name"
+          />
+        </label> */}
 
         <label>
           <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
+          <input
+            onChange={handleChange}
+            defaultValue={defaultValues.difficulty}
+            type="range"
+            min={1}
+            max={5}
+            name="difficulty"
+          />
         </label>
 
         <label>
-          <button type="submit">Add Item</button>
+          <Button type="submit" color="teal" radius="md" size="lg">
+            Add Item
+          </Button>
         </label>
       </form>
 
-      {list.map(item => (
+      {list.map((item) => (
         <div key={item.id}>
           <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+          <p>
+            <small>Assigned to: {item.assignee}</small>
+          </p>
+          <p>
+            <small>Difficulty: {item.difficulty}</small>
+          </p>
+          <div onClick={() => toggleComplete(item.id)}>
+            Complete: {item.complete.toString()}
+          </div>
+          <Button
+            type="button"
+            onClick={() => deleteItem(item.id)}
+            color="red"
+            radius="xs"
+            size="xs"
+          >
+            Delete Item
+          </Button>
           <hr />
         </div>
       ))}
-
     </>
   );
 };
